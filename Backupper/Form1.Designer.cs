e8bb5d67436b4c2d591bd5143f392fcf870e6b0c
Sebastian Becker <mail@sbecker.de.com>
2020-02-18 22:57:01 +0100

    Removed Backup* from gitingore
    Probably better considering all the source is in a folder with that name

---
+++ Backupper/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Backupper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.lsbBackupContent = new System.Windows.Forms.ListBox();
+            this.btnRemoveEntry = new System.Windows.Forms.Button();
+            this.btnZipFiles = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.FileProgress = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddFolder
+            // 
+            this.btnAddFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.Size = new System.Drawing.Size(97, 23);
+            this.btnAddFolder.TabIndex = 0;
+            this.btnAddFolder.Text = "Add Folder";
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            this.btnAddFolder.Click += new System.EventHandler(this.BtnAddFolder_Click);
+            // 
+            // lsbBackupContent
+            // 
+            this.lsbBackupContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsbBackupContent.FormattingEnabled = true;
+            this.lsbBackupContent.Location = new System.Drawing.Point(12, 41);
+            this.lsbBackupContent.Name = "lsbBackupContent";
+            this.lsbBackupContent.Size = new System.Drawing.Size(1111, 446);
+            this.lsbBackupContent.TabIndex = 1;
+            // 
+            // btnRemoveEntry
+            // 
+            this.btnRemoveEntry.Location = new System.Drawing.Point(218, 12);
+            this.btnRemoveEntry.Name = "btnRemoveEntry";
+            this.btnRemoveEntry.Size = new System.Drawing.Size(97, 23);
+            this.btnRemoveEntry.TabIndex = 2;
+            this.btnRemoveEntry.Text = "Remove Entry";
+            this.btnRemoveEntry.UseVisualStyleBackColor = true;
+            this.btnRemoveEntry.Click += new System.EventHandler(this.btnRemoveFolder_Click);
+            // 
+            // btnZipFiles
+            // 
+            this.btnZipFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnZipFiles.Location = new System.Drawing.Point(1048, 508);
+            this.btnZipFiles.Name = "btnZipFiles";
+            this.btnZipFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnZipFiles.TabIndex = 3;
+            this.btnZipFiles.Text = "Zip Files";
+            this.btnZipFiles.UseVisualStyleBackColor = true;
+            this.btnZipFiles.Click += new System.EventHandler(this.btnZipFiles_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtPassword.Location = new System.Drawing.Point(74, 510);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(156, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 513);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password:";
+            // 
+            // FileProgress
+            // 
+            this.FileProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileProgress.Location = new System.Drawing.Point(293, 508);
+            this.FileProgress.Name = "FileProgress";
+            this.FileProgress.Size = new System.Drawing.Size(749, 23);
+            this.FileProgress.TabIndex = 7;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(1048, 507);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Visible = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 513);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Progress:";
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(115, 12);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(97, 23);
+            this.btnFile.TabIndex = 12;
+            this.btnFile.Text = "Add File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 543);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.FileProgress);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnZipFiles);
+            this.Controls.Add(this.btnRemoveEntry);
+            this.Controls.Add(this.lsbBackupContent);
+            this.Controls.Add(this.btnAddFolder);
+            this.Name = "frmMain";
+            this.Text = "Backupper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.ListBox lsbBackupContent;
+        private System.Windows.Forms.Button btnRemoveEntry;
+        private System.Windows.Forms.Button btnZipFiles;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.ProgressBar FileProgress;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFile;
+    }
+}
+
---
+++ Backupper/Form1.cs
@@ -0,0 +1,175 @@
+﻿using Backupper.Properties;
+using Ionic.Zip;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Backupper
+{
+    public partial class frmMain : Form
+    {
+        private volatile bool Canceled = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            if (Settings.Default.Folders == null)
+            {
+                Settings.Default.Folders = new List<string>();
+            }
+
+            if (Settings.Default.Files == null)
+            {
+                Settings.Default.Files = new List<string>();
+            }
+
+            UpdateBackupContentList();
+        }
+
+        private void UpdateBackupContentList()
+        {
+            lsbBackupContent.Items.Clear();
+            foreach (var folder in Settings.Default.Folders)
+            {
+                lsbBackupContent.Items.Add(folder);
+            }
+            foreach (var file in Settings.Default.Files)
+            {
+                lsbBackupContent.Items.Add(file);
+            }
+        }
+
+        private void BtnAddFolder_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new CommonOpenFileDialog())
+            {
+                dlg.IsFolderPicker = true;
+                dlg.Multiselect = true;
+                if (dlg.ShowDialog() != CommonFileDialogResult.Ok)
+                {
+                    return;
+                }
+
+                Settings.Default.Folders.AddRange(dlg.FileNames);
+                Settings.Default.Save();
+                UpdateBackupContentList();
+            }
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new CommonOpenFileDialog())
+            {
+                dlg.IsFolderPicker = false;
+                dlg.Multiselect = true;
+                if (dlg.ShowDialog() != CommonFileDialogResult.Ok)
+                {
+                    return;
+                }
+
+                Settings.Default.Files.AddRange(dlg.FileNames);
+                Settings.Default.Save();
+                UpdateBackupContentList();
+            }
+        }
+
+        private void btnRemoveFolder_Click(object sender, EventArgs e)
+        {
+            if (lsbBackupContent.SelectedIndex < 0)
+            {
+                return;
+            }
+            Settings.Default.Folders.Remove(lsbBackupContent.SelectedItem.ToString());
+            Settings.Default.Files.Remove(lsbBackupContent.SelectedItem.ToString());
+            Settings.Default.Save();
+            UpdateBackupContentList();
+        }
+
+        private string SelectOutputFile()
+        {
+            using (var dlg = new SaveFileDialog())
+            {
+                dlg.FileName = "Backup";
+                dlg.Filter = "Zip File (*.zip)|*.zip";
+                if (dlg.ShowDialog() != DialogResult.OK)
+                {
+                    return null;
+                }
+                return dlg.FileName;
+            }
+        }
+
+        private void btnZipFiles_Click(object sender, EventArgs e)
+        {
+            var file_name = SelectOutputFile();
+            if (string.IsNullOrWhiteSpace(file_name))
+            {
+                return;
+            }
+
+            Canceled = false;
+            btnCancel.Visible = true;
+            btnZipFiles.Visible = false;
+
+            MessageBox.Show("Make sure Thunderbird is closed before you continue.");
+
+            Task.Run(() =>
+            {
+                using (var zip_file = new ZipFile())
+                {
+                    zip_file.UseZip64WhenSaving = Zip64Option.Always;
+                    zip_file.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression;
+                    zip_file.CompressionMethod = CompressionMethod.BZip2;
+                    zip_file.Password = txtPassword.Text;
+                    zip_file.MaxOutputSegmentSize = 1024 * 1024 * 200;
+
+                    foreach (var folder in Settings.Default.Folders)
+                    {
+                        zip_file.AddDirectory(folder, Path.GetFileName(folder));
+                    }
+
+                    zip_file.AddFiles(Settings.Default.Files);
+
+                    zip_file.SaveProgress += (s, args) =>
+                    {
+                        args.Cancel = Canceled;
+                        if (args.EventType != ZipProgressEventType.Saving_AfterWriteEntry)
+                        {
+                            return;
+                        }
+                        FileProgress.Invoke(new Action(() =>
+                        {
+                            FileProgress.Maximum = args.EntriesTotal;
+                            FileProgress.Value = args.EntriesSaved;
+                        }));
+                    };
+
+                    zip_file.Save(file_name);
+                }
+            }).ContinueWith((t) =>
+            {
+                this.Invoke(new Action(() =>
+                {
+                    MessageBox.Show("Done!");
+                }));
+            });
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Canceled = true;
+            btnCancel.Visible = false;
+            btnZipFiles.Visible = true;
+        }
+    }
+}
