066bdf0fedcb01777fbc2953d0384da5d97f3d95
Sebastian Becker <mail@sbecker.de.com>
2024-05-18 20:45:32 +0200

    Remove unused usings

--- Backupper/Form1.cs
+++ Backupper/Form1.cs
@@ -4,13 +4,7 @@ using Microsoft.WindowsAPICodePack.Dialogs;
 using Microsoft.WindowsAPICodePack.Taskbar;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
